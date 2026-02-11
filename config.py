44817dc39876655a7c2aafb945369713c05208ce
Madhav Mendiratta <madhavmendiratta08@gmail.com>
2026-02-11 23:12:30 +0530

    feat: add cache, API key and playwright environment setup

--- config.py
+++ config.py
@@ -11,3 +11,19 @@ def url_to_sha256_filename(url: str, extension: str = "webm") -> str:
     base_name = base_name.replace(":", "_").replace("/", "_")
     filename = f"{base_name}_{sha256_hash}.{extension}"
     return filename
+
+def setup_configurations():
+    load_env_file()
+
+    cache = Cache("./cache")
+    cache_expiration_seconds = int(os.getenv("CACHE_EXPIRATION_SECONDS", 3600))
+
+    playwright_browsers_path = os.getenv("PLAYWRIGHT_BROWSERS_PATH", "0")
+    if playwright_browsers_path != "0":
+        os.environ["PLAYWRIGHT_BROWSERS_PATH"] = playwright_browsers_path
+
+    api_key = os.environ.get("API_KEY", "none")
+    security = HTTPBearer(auto_error=False)
+
+    return cache, cache_expiration_seconds, security, api_key
+
